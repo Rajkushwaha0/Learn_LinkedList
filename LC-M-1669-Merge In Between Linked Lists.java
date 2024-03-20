ca12f0542056963ac5bc8f138dc1564a096718e1
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2024-03-20 10:12:42 +0530

    Update LC-M-1669-Merge In Between Linked Lists.java

--- LC-M-1669-Merge In Between Linked Lists.java
+++ LC-M-1669-Merge In Between Linked Lists.java
@@ -1,3 +1,34 @@
+//more clear
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode ans = dummy;
+        ListNode curr = list1;
+        int count=0;
+        while(curr!=null){
+            if(curr!=null && count==a){
+                while(list2!=null){
+                    dummy.next = new ListNode(list2.val);
+                    list2=list2.next;
+                    dummy = dummy.next;
+                }
+                while(curr!=null && (count)<=b){
+                    curr=curr.next;
+                    count++;
+                }
+            }
+            else{
+                dummy.next = new ListNode(curr.val);
+                curr= curr.next;
+                dummy = dummy.next;
+                count++;   
+            }
+        }
+        return ans.next;
+
+    }
+}
+
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         ListNode dummy = new ListNode(-1);
